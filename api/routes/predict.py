133c62d4009ff1ac2938d23e1e2dfbaec2cf9d5a
Abdul Mutakabbir <47092538+SammSum1@users.noreply.github.com>
2023-04-30 21:10:29 -0400

    added model paths

--- api/routes/predict.py
+++ api/routes/predict.py
@@ -5,8 +5,9 @@ import joblib
 router = APIRouter()
 
 model = {}
-# model[ModelType.RF] = joblib.load("rf.joblib")
-# model[ModelType.DL] = joblib.load("dl.joblib")
+model[ModelType.RF] = joblib.load("./models/model_random_forest.joblib")
+model[ModelType.DL] = joblib.load("./models/model_deep_learning.joblib")
+DL_STATS = joblib.load("./models/dataset_stats_deep_learning.joblib")
 
 @router.get(
         "/predict", 
@@ -15,7 +16,7 @@ model = {}
         By default the model is set to "Random Forest". 
         Two input features (vol_moving_avg, adj_close_rolling_med) need to be provide to the model to make predictions.'''
     )
-async def predict(vol_moving_avg:float, adj_close_rolling_med:float, model_type:ModelType=ModelType.DL)->int:
+async def predict(vol_moving_avg:float, adj_close_rolling_med:float, model_type:ModelType=ModelType.RF)->int:
     if vol_moving_avg < 0:
         raise HTTPException(
             status_code=422, 
@@ -26,5 +27,11 @@ async def predict(vol_moving_avg:float, adj_close_rolling_med:float, model_type:
             status_code=422,
             detail="adj_close_rolling_med should be greater than 0 (zero)."
         )
-    prediction = model[model_type].predict()
+    X = [[vol_moving_avg, adj_close_rolling_med]]
+    model = model[model_type]
+    prediction = None
+    if model_type == ModelType.RF:
+        prediction = model.predict(X)[0]
+    elif model_type == ModelType.DL:
+        prediction = model.predict(X)[0]
     return prediction
